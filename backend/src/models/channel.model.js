0fb9618dc623fc20322965794bfc86c75ff8743d
ccupalg <ccupalg@gmail.com>
2025-05-18 15:37:36 +0530

    image datatype is made as an Array to support multiple images

--- backend/src/models/channel.model.js
+++ backend/src/models/channel.model.js
@@ -6,6 +6,30 @@ const channelSchema = new mongoose.Schema(
 		description: { type: String },
 		members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 		createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+		status: {
+			enum: ["ACTIVE", "FREEZE", "DELETE"],
+			type: String,
+			default: "ACTIVE",
+		},
+		pinnedChannel: [
+			{
+				pinnedFor: {
+					enum: ["SELF", "GLOBAL"],
+					default: "SELF",
+					type: String,
+				},
+				pinnedBy: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "User",
+				},
+			},
+		],
+		pinnedMessages: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Message",
+			},
+		],
 	},
 	{ timestamps: true }
 );
--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,30 +1,33 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
-  {
-    senderId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    receiverId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    text: {
-      type: String,
-    },
-    image: {
-      type: String,
-    },
-    messageType: {  // RELATED FEATURE: use "messageType" to differentiate between user and channel messages while notifying it's receiver
-      type: String,
-      enum: ["USER", "CHANNEL"],
-      default: "USER",
-    }
-  },
-  { timestamps: true }
+	{
+		senderId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		receiverId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		text: {
+			type: String,
+		},
+		image: [
+			{
+				type: String,
+			},
+		],
+		messageType: {
+			// RELATED FEATURE: use "messageType" to differentiate between user and channel messages while notifying it's receiver
+			type: String,
+			enum: ["USER", "CHANNEL", "SYSTEM"],
+			default: "USER",
+		},
+	},
+	{ timestamps: true }
 );
 
 const Message = mongoose.model("Message", messageSchema);
